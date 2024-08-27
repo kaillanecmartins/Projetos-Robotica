ff42feea92e11fba1fbba5ff2768767e2697cc8d
Kaillane Martins <kaillanemartins18@gmail.com>
2024-08-27 14:31:11 -0300

    inlcuindo configurações do ultrassonico

--- ultrassonico_pi/ultrassonico_pi.ino
+++ ultrassonico_pi/ultrassonico_pi.ino
@@ -1,4 +1,4 @@
-#include <Ultrasonic.h>
+//#include <Ultrasonic.h>
 // portas ultrassonico
 #define p_echo 20
 #define p_trig 21
@@ -7,16 +7,30 @@
 #define p_scl 18
 #define p_sda 19
 
+unsigned long tempo;
+double distancia;
+
 void setup() {
   Serial.begin(9600);
   pinMode(p_echo, INPUT);
-  pinMode(p_trig, INPUT);
+  pinMode(p_trig, OUTPUT);
   pinMode(p_scl, OUTPUT);
   pinMode(p_sda, OUTPUT);
+  digitalWrite(p_trig, LOW);
 
 }
 
 void loop() {
-  
+  digitalWrite(p_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(p_trig, LOW);
+
+  tempo = pulseIn(p_echo, HIGH);
+
+  distancia = tempo/58;
+
+  Serial.println(distancia);
+
+  delay(500);  
 
 }
