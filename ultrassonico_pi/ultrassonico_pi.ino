aa0a3b11c6220f9cbdc19499583ead2c67a502a3
Kaillane Martins <kaillanemartins18@gmail.com>
2024-08-27 14:22:09 -0300

    inlcuindo biblioteca

--- ultrassonico_pi/ultrassonico_pi.ino
+++ ultrassonico_pi/ultrassonico_pi.ino
@@ -1,4 +1,4 @@
-
+#include <Ultrasonic.h>
 // portas ultrassonico
 #define p_echo 20
 #define p_trig 21
@@ -8,7 +8,11 @@
 #define p_sda 19
 
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(p_echo, INPUT);
+  pinMode(p_trig, INPUT);
+  pinMode(p_scl, OUTPUT);
+  pinMode(p_sda, OUTPUT);
 
 }
 
