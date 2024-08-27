148d169eb4b363099e6047629d7f2f1ca556fa33
Kaillane Martins <kaillanemartins18@gmail.com>
2024-08-27 14:44:31 -0300

    inlcuindo biblioteca LEDRGB

--- ultrassonico_pi/ultrassonico_pi.ino
+++ ultrassonico_pi/ultrassonico_pi.ino
@@ -1,4 +1,5 @@
 //#include <Ultrasonic.h>
+
 // portas ultrassonico
 #define p_echo 20
 #define p_trig 21
@@ -10,6 +11,78 @@
 unsigned long tempo;
 double distancia;
 
+#define LedR 17
+#define LedG 16
+#define LedB 15
+
+//biblioteca Github: https://github.com/samueldesaa/Robotic-Projects/blob/main/Arduino/ledPWM-Ultrasonic-Classes.ino
+
+class ledRGB {
+  int R, G, B;
+
+public:
+  //Construtor do LED
+  ledRGB(int r, int g, int b) {
+    R = r;
+    G = g;
+    B = b;
+    pinMode(R, OUTPUT);
+    pinMode(G, OUTPUT);
+    pinMode(B, OUTPUT);
+  }
+  //Função de cor aleatória
+  void randColor() {
+    analogWrite(R, random(256));
+    analogWrite(G, random(256));
+    analogWrite(B, random(256));
+  }
+  //Função de escolher cor específica
+  void color(int r, int g, int b) {
+    analogWrite(R, r);
+    analogWrite(G, g);
+    analogWrite(B, b);
+  }
+  void color(int r, int g, int b, int del) {
+    analogWrite(R, r);
+    analogWrite(G, g);
+    analogWrite(B, b);
+    delay(del);
+  }
+  //Função de ligar todas as cores
+  void white() {
+    digitalWrite(R, HIGH);
+    digitalWrite(G, HIGH);
+    digitalWrite(B, HIGH);
+  }
+  //Led vermelho
+  void red() {
+    digitalWrite(R, HIGH);
+    digitalWrite(G, LOW);
+    digitalWrite(B, LOW);
+  }
+  //Led verde
+  void green() {
+    digitalWrite(R, LOW);
+    digitalWrite(G, HIGH);
+    digitalWrite(B, LOW);
+  }
+  //Led azul
+  void blue() {
+    digitalWrite(R, LOW);
+    digitalWrite(G, LOW);
+    digitalWrite(B, HIGH);
+  }
+  //Desligar
+  void off() {
+    digitalWrite(R, LOW);
+    digitalWrite(G, LOW);
+    digitalWrite(B, LOW);
+  }
+};
+
+//Determinado as portas do led
+ledRGB led(LedR, LedG, LedB);
+
 void setup() {
   Serial.begin(9600);
   pinMode(p_echo, INPUT);
@@ -17,7 +90,7 @@ void setup() {
   pinMode(p_scl, OUTPUT);
   pinMode(p_sda, OUTPUT);
   digitalWrite(p_trig, LOW);
-
+  randomSeed(analogRead(0));
 }
 
 void loop() {
