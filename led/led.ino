916bf131bd39f4d8bd892b8cf9d69e4f0b0156f1
Kaillane Martins <kaillanemartins18@gmail.com>
2024-09-10 08:07:38 -0300

    incluindo exemplo led

--- led/led.ino
+++ led/led.ino
@@ -2,13 +2,13 @@
 //
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(1, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
+  digitalWrite(1, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
+  digitalWrite(1, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
---
+++ teste_raspberry/teste.py
@@ -0,0 +1,10 @@
+from machine import Pin
+from time import sleep
+
+pin = Pin("LED", Pin.OUT)
+
+while True:
+    pin.toggle()
+    sleep(1)
+
+    
