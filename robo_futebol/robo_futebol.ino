424a34fc99d7decee36f0b2909d82535e38aa833
Kaillane Martins <kaillanemartins18@gmail.com>
2024-11-08 11:44:44 -0300

    iniciando robo com módulo bluetooth HC-05

---
+++ robo_futebol/robo_futebol.ino
@@ -0,0 +1,69 @@
+#include <AFMotor.h> // Biblioteca do motor shield
+
+//********************Criação dos objetos********************
+
+AF_DCMotor motor1(1); // Motor esquerdo
+AF_DCMotor motor2(2); // Motor direito
+
+void setup() {
+  // Configuração de velocidade dos motores
+  motor1.setSpeed(255); // Velocidade vai de 0 a 255
+  motor2.setSpeed(255);
+  //Iniciar monitor serial
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  char c = Serial.read();
+
+  switch (c) {
+    case 'F':
+      frente();
+      break;
+    case 'A':
+      frente();
+      break;
+    case 'L':
+      esquerda();
+      break;
+    case 'R':
+      direita();
+      break;
+    case 'B':
+      tras();
+      break;
+    case 'S':
+      para();
+      break;
+  }
+
+}
+
+//********************Movimenta o robô para frente********************
+
+void frente() {
+  motor1.run(FORWARD); // Motor esquerdo
+  motor2.run(FORWARD); // Motor direito
+}
+
+//********************Movimenta o robô para trás********************
+
+void tras() {
+  motor1.run(BACKWARD); // Motor esquerdo
+  motor2.run(BACKWARD); // Motor direito
+}
+
+//********************Parada dos motores********************
+
+void para() {
+  motor1.run(RELEASE); // Motor esquerdo
+  motor2.run(RELEASE); // Motor direito
+}
+
+//********************Movimenta o robô para esquerda********************
+
+void esquerda() {
+  motor1.run(FORWARD); // Motor esquerdo
+  motor2.run(BACKWARD); // Motor direito
+}
