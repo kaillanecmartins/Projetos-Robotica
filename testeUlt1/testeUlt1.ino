440e240f64347576026fa0ff79aeab80803c507b
Kaillane Martins <kaillanemartins18@gmail.com>
2026-03-27 20:34:06 -0300

    teste servo e sensor

--- testeUlt1/testeUlt1.ino
+++ testeUlt1/testeUlt1.ino
@@ -14,8 +14,8 @@ BnrOneAPlus one;  // object to control the Bot'n Roll ONE A+
 Servo servoA;
 Ultrasonic ult1(A5, A4);
 
-int angAberto = 20;
-int angFechado = 180;
+int angAberto = 120;
+int angFechado = 0;
 
 // Transition value between white and black
 // Valor de transição entre branco e preto
