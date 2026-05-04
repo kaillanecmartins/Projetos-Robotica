933f35e1c0a1946eec7d9645e0da8bf124a3a435
Kaillane Martins <kaillanemartins18@gmail.com>
2026-05-04 20:43:44 -0300

    Incluindo sensor de ruído

--- Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
+++ Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
@@ -5,12 +5,14 @@
 #define B_pin 21
 
 #define DHT_pin 15
-#define KY_pin 23
+#define KY_pin 2
 #define LDR_pin 4
 
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
 
 DHT dht(DHT_pin, DHTTYPE);
+
+bool estado = 0; //verifica se teve ruido ou não
 
 void setup(){
   Serial.begin(9600);
@@ -30,6 +32,7 @@ void loop(){
   float temp = dht.readTemperature();
   float umidade = dht.readHumidity();
   int luminosidade = analogRead(LDR_pin);
+  int barulho = analogRead(KY_pin);
   
   Serial.print("Temperatura: ");
   Serial.println(temp);
@@ -37,6 +40,18 @@ void loop(){
   Serial.println(umidade);
   Serial.print("Luminosidade: ");
   Serial.println(luminosidade);
+  Serial.print("Barulho: ");
+  Serial.println(barulho);
 
+  if(barulho < 600){
+    digitalWrite(R_pin, HIGH);
+    digitalWrite(G_pin, LOW);
+    digitalWrite(B_pin, LOW);
+  }
+  else{
+    digitalWrite(R_pin, LOW);
+    digitalWrite(G_pin, HIGH);
+    digitalWrite(B_pin, LOW);
+  }
   delay(1000);
 }
