bdeb9d5a8d30dfd82182b4579381eb554d730fd9
Kaillane Martins <kaillanemartins18@gmail.com>
2026-05-21 20:31:12 -0300

    incluindo sensores IR

--- Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
+++ Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
@@ -1,13 +1,16 @@
 #include <DHT.h>
 
-#define R_pin 18
-#define G_pin 19
-#define B_pin 21
+#define R_pin1 4
+#define G_pin1 18
+#define R_pin2 19
+#define G_pin2 21
 
 #define DHT_pin 15
-#define KY_pin 2
-#define LDR_pin 4
-#define PIR_pin 22
+//#define KY_pin 2
+//#define LDR_pin 4
+//#define PIR_pin 22
+#define IR1_pin 22
+#define IR2_pin 15
 
 #define DHTTYPE DHT11
 
@@ -17,14 +20,17 @@ bool estado = 0; //verifica se teve ruido ou não
 
 void setup(){
   Serial.begin(9600);
-  pinMode(R_pin, OUTPUT);
-  pinMode(G_pin, OUTPUT);
-  pinMode(B_pin, OUTPUT);
+  pinMode(R_pin1, OUTPUT);
+  pinMode(G_pin1, OUTPUT);
+  pinMode(R_pin2, OUTPUT);
+  pinMode(G_pin2, OUTPUT);
 
   pinMode(DHT_pin, INPUT);
-  pinMode(KY_pin, INPUT);
-  pinMode(LDR_pin, INPUT);
-  pinMode(PIR_pin, INPUT);
+  pinMode(IR1_pin, INPUT);
+  pinMode(IR2_pin, INPUT);
+  //pinMode(KY_pin, INPUT);
+ // pinMode(LDR_pin, INPUT);
+  //pinMode(PIR_pin, INPUT);
 
   dht.begin();
 }
@@ -33,43 +39,61 @@ void loop(){
 
   float temp = dht.readTemperature();
   float umidade = dht.readHumidity();
-  int luminosidade = analogRead(LDR_pin);
-  int barulho = analogRead(KY_pin);
-  int presenca = digitalRead(PIR_pin);
+  int ir1 = digitalRead(IR1_pin);
+  int ir2 = digitalRead(IR2_pin);
+  //int luminosidade = analogRead(LDR_pin);
+  //int barulho = analogRead(KY_pin);
+  //int presenca = digitalRead(PIR_pin);
   
   Serial.print("Temperatura: ");
   Serial.println(temp);
   Serial.print("Umidade: ");
   Serial.println(umidade);
-  Serial.print("Luminosidade: ");
-  Serial.println(luminosidade);
-  Serial.print("Barulho: ");
+  Serial.print("IR simples: ");
+  Serial.println(ir1);
+  Serial.print("IR complexo: ");
+  Serial.println(ir2);
+  /*Serial.print("Luminosidade: ");
+  Serial.println(luminosidade);*/
+  /*Serial.print("Barulho: ");
   Serial.println(barulho);
   Serial.print("Presença: ");
-  Serial.println(presenca);
+  Serial.println(presenca);*/
 
-  /*
-  if(barulho < 600){
-    digitalWrite(R_pin, HIGH);
-    digitalWrite(G_pin, LOW);
-    digitalWrite(B_pin, LOW);
-  }
-  else{
-    digitalWrite(R_pin, LOW);
-    digitalWrite(G_pin, HIGH);
-    digitalWrite(B_pin, LOW);
+  /*if(barulho > 700){
+    LED_RG1(0,1);
+  }else{
+    LED_RG1(1,0);
   }*/
 
-  if(presenca){
-    LED_RGB(1,0,0);
+/* if(!presenca){
+    LED_RG2(1,0);
   }else{
-    LED_RGB(0,1,0);
+    LED_RG2(0,1);
+  }*/
+
+  //detecção com ir simples
+  if(ir1){
+    LED_RG1(1, 0);
+  }else{
+    LED_RG1(0,1);
   }
-  delay(1000);
+
+  //detecção com ir complexo
+  if(ir2){
+    LED_RG2(1, 0);
+  }else{
+    LED_RG2(0,1);
+  }
+
+  delay(500);
 }
 
-void LED_RGB(int modeR, int modeG, int modeB){
-    digitalWrite(R_pin, modeR);
-    digitalWrite(G_pin, modeG);
-    digitalWrite(B_pin, modeB);
+void LED_RG1(int modeR, int modeG){
+    digitalWrite(R_pin1, modeR);
+    digitalWrite(G_pin1, modeG);
+}
+void LED_RG2(int modeR, int modeG){
+    digitalWrite(R_pin2, modeR);
+    digitalWrite(G_pin2, modeG);
 }
