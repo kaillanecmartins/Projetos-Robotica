919dd45daa058d8d3526f2f559f7f1d684be727b
Kaillane Martins <kaillanemartins18@gmail.com>
2026-06-02 20:23:46 -0300

    incluindo todos os sensores

--- Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
+++ Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas/Kaillane_ProjetoIoT_MonitoramentoDeBibliotecas.ino
@@ -1,13 +1,10 @@
 #include <DHT.h>
 
-#define R_pin1 5
-#define G_pin1 18
-#define R_pin2 19
-#define G_pin2 21
+#define led1 18
+#define led2 19
 
 #define DHT_pin 15
-//#define KY_pin 2
-//#define LDR_pin 4
+#define KY_pin 2
 //#define PIR_pin 22
 #define IR1_pin 22
 #define IR2_pin 4
@@ -20,15 +17,13 @@ bool estado = 0; //verifica se teve ruido ou não
 
 void setup(){
   Serial.begin(9600);
-  pinMode(R_pin1, OUTPUT);
-  pinMode(G_pin1, OUTPUT);
-  pinMode(R_pin2, OUTPUT);
-  pinMode(G_pin2, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 
   pinMode(DHT_pin, INPUT);
   pinMode(IR1_pin, INPUT);
   pinMode(IR2_pin, INPUT);
-  //pinMode(KY_pin, INPUT);
+  pinMode(KY_pin, INPUT);
  // pinMode(LDR_pin, INPUT);
   //pinMode(PIR_pin, INPUT);
 
@@ -41,8 +36,7 @@ void loop(){
   float umidade = dht.readHumidity();
   int ir1 = digitalRead(IR1_pin);
   int ir2 = digitalRead(IR2_pin);
-  //int luminosidade = analogRead(LDR_pin);
-  //int barulho = analogRead(KY_pin);
+  int barulho = analogRead(KY_pin);
   //int presenca = digitalRead(PIR_pin);
   
   Serial.print("Temperatura: ");
@@ -53,47 +47,23 @@ void loop(){
   Serial.println(ir1);
   Serial.print("IR complexo: ");
   Serial.println(ir2);
-  /*Serial.print("Luminosidade: ");
-  Serial.println(luminosidade);*/
-  /*Serial.print("Barulho: ");
+  Serial.print("Barulho: ");
   Serial.println(barulho);
-  Serial.print("Presença: ");
+  /*Serial.print("Presença: ");
   Serial.println(presenca);*/
-
-  /*if(barulho > 700){
-    LED_RG1(0,1);
-  }else{
-    LED_RG1(1,0);
-  }*/
-
-/* if(!presenca){
-    LED_RG2(1,0);
-  }else{
-    LED_RG2(0,1);
-  }*/
 
   //detecção com ir simples
   if(ir1 == 1){
-    LED_RG1(1, 0);
+    digitalWrite(led1, HIGH);
   }else{
-    LED_RG1(0,1);
+    digitalWrite(led1, LOW);
   }
 
   //detecção com ir complexo
   if(ir2 == 1){
-    LED_RG2(1, 0);
+    digitalWrite(led2, HIGH);
   }else{
-    LED_RG2(0,1);
+    digitalWrite(led2, LOW);
   }
-
   delay(500);
-}
-
-void LED_RG1(int modeR, int modeG){
-    digitalWrite(R_pin1, modeR);
-    digitalWrite(G_pin1, modeG);
-}
-void LED_RG2(int modeR, int modeG){
-    digitalWrite(R_pin2, modeR);
-    digitalWrite(G_pin2, modeG);
 }
